5673472a33d45825f68e6920d0fdceb16c487a13
NemesisXB <485436+NemesisXB@users.noreply.github.com>
2024-03-19 22:05:24 +0200

    Fix issue with mismatched function definition

--- include/owb_rmt.h
+++ include/owb_rmt.h
@@ -73,7 +73,7 @@ OneWireBus bus;                               ///< OneWireBus instance (see owb.
  * @return OneWireBus *, pass this into the other OneWireBus public API functions
  */
 OneWireBus* owb_rmt_initialize(owb_rmt_driver_info * info, gpio_num_t gpio_num,
-                               rmt_channel_t tx_channel, rmt_channel_t rx_channel);
+                               int tx_channel, int rx_channel);
 
 
 /**
